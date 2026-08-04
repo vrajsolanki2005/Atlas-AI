07eefea6b62377142d474b8dc8db2c7e4eedcf51
vrajsolanki2005 <144586038+vrajsolanki2005@users.noreply.github.com>
2026-08-05 01:30:52 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         dialect: "mysql",
-        logging: console.log
+        logging: process.env.NODE_ENV === "development" ? console.log : false
     }
 );
 
